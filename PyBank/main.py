c73e522f138f2aa4773a448bbbc72d09548fd07d
Levi Rdz <angelevirdz@gmail.com>
2020-08-16 16:07:11 -0500

    made some more progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,12 +5,13 @@ import os
 import csv 
 
 # Assign a variable to the file getting read
-path = os.path.join("Resources", "budget_data.csv")
+path = os.path.join("budget_data.csv")
 
 r_index = 1
 months = 0 
 total = 0
 change_date = []
+earnings = []
 
 
 # "Open" the variable storing the file
@@ -29,3 +30,12 @@ with open(path) as csvfile:
     for row in csv_reader: 
         months += 1
         change_date.append(row[0])
+
+        total = total + int(row[1])
+
+        # Profit/Loss avg. change
+        income_change = int(row[1]) - income
+        earnings.append(income_change)
+        income = int(row[1])
+    
+    avg_income_chage = round((sum(earnings)/len(earnings)), 2)
